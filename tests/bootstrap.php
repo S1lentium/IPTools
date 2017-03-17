5ce333ec82a609120764f537ace68276ce558ae7
Safarov Alisher <3dflex@gmail.com>
2017-03-17 10:10:41 +0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
 
-// backward compatibility for php 5.5 and low (with phpunit < v.6)
-if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
-  class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
 
 require __DIR__ . '/../src/PropertyTrait.php';
